237ce0e4fcbed5ce19533e8b4cfaf417d7df2c7a
Rafael Matos <skyrafael@hotmail.com>
2018-05-03 01:30:00 -0300

    inicial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jad';
+  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
+  leia = {name: 'Leia', isJedi: false};
+  han = {name: 'Han SOlo', isJedi: false};
 }
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+import {Student} from './student.model';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  @Input() student: Student;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
